13f1bf3fe1066e781a96fc3cce4f4bbf27c516e9
eren akyıldız <erenakyildiz@sabanciuniv.edu>
2022-08-14 14:31:00 +0300

    added collection page's necessary components.

---
+++ lib/decoration/CollectionPageDecoration.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+const collectionNameBoxColor = Color(0x2CFFFFFF);
+const collectionOwnerBoxColor = Color(0x2CFFFFFF);
+const collectionNameTextColor = Colors.white;
+var collectionNameBoxDecoration = BoxDecoration(
+  color: collectionNameBoxColor,
+  borderRadius: BorderRadius.all(Radius.circular(20)),
+
+);
+var collectionNameTextStyle = GoogleFonts.roboto(
+  fontWeight: FontWeight.w700,
+  color: collectionNameTextColor,
+  fontSize: 16,
+
+);
+var collectionDescriptionTextStyle = GoogleFonts.roboto(
+  fontWeight: FontWeight.w400,
+  color: collectionNameTextColor,
+  fontSize: 16,
+
+);
+var collectionOwnerBoxDecoration = BoxDecoration(
+  color: collectionOwnerBoxColor,
+  borderRadius: BorderRadius.all(Radius.circular(20)),
+
+);
+var collectionOwnerTextStyle = GoogleFonts.roboto(
+  fontWeight: FontWeight.w400,
+  color: collectionNameTextColor,
+  fontSize: 16,
+
+);
--- lib/pages/CollectionPage.dart
+++ lib/pages/CollectionPage.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:properly_made_nft_market/helpers/UserHelper.dart';
 import 'package:properly_made_nft_market/models/NftCollection.dart';
+import 'package:properly_made_nft_market/decoration/AnimatedGradient.dart';
+import 'package:properly_made_nft_market/decoration/CollectionPageDecoration.dart' as decoration;
 
+import '../models/User.dart';
 class CollectionPage extends StatefulWidget {
   const CollectionPage({Key? key, required this.collectionInfo}) : super(key: key);
   final NFTCollection collectionInfo;
@@ -9,16 +13,148 @@ class CollectionPage extends StatefulWidget {
 }
 
 class _CollectionPageState extends State<CollectionPage> {
+  bool expandPressed = false;
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Image(image: NetworkImage(widget.collectionInfo.collectionImage))
+    return Scaffold(
+      body:
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+
+            children: [
+              Positioned(
+                child: AnimatedGradient(),
+              ),
+              Positioned(
+                child: SingleChildScrollView(
+                  physics: BouncingScrollPhysics(),
+                  child: Column(
+
+                    children: [
+                      SafeArea(
+                        child: Padding(
+                            padding: EdgeInsets.all(20),
+                            child:
+                            ClipRRect(
+                              borderRadius: BorderRadius.all(Radius.circular(20)),
+                             child:  Image(image: NetworkImage(widget.collectionInfo.collectionImage)),
+                            )
 
 
-        ],
-      ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(20),
+                        padding: EdgeInsets.all(20),
+                        decoration: decoration.collectionNameBoxDecoration,
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  widget.collectionInfo.name,
+                                  style: decoration.collectionNameTextStyle,
+                                ),
+                              ],
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(10),
+                            ),
+                            
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Watchlist: " + widget.collectionInfo.numLikes.toString(),
+                                  style: decoration.collectionNameTextStyle,
+                                ),
+                                Text(
+                                  "Likes: " + widget.collectionInfo.NFTLikes.toString(),
+                                  style: decoration.collectionNameTextStyle,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: widget.collectionInfo.description != null
+                            ?
+                        (widget.collectionInfo.description!.length <= 100 || expandPressed) ? Text(
+                          "Description: \n\n" + widget.collectionInfo.description!,
+                          style: decoration.collectionDescriptionTextStyle,
+                        )
+                        :
+                        GestureDetector(
+                          child: Text("Description: \n\n" + widget.collectionInfo.description!.substring(0,100)+ "...",
+                          style: decoration.collectionDescriptionTextStyle,)
+                            ,
+                          onTap: ()=> setState(() {
+                            expandPressed = true;
+                          })
+                        )
+
+
+
+
+                            :
+                        Text("No description provided by owner",
+                          style: decoration.collectionDescriptionTextStyle,
+
+                        ),
+                      ),
+                      Container(
+                        decoration: decoration.collectionOwnerBoxDecoration,
+                        margin: EdgeInsets.all(20),
+                        padding: EdgeInsets.all(20),
+                        width: MediaQuery.of(context).size.width,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Created by: " + widget.collectionInfo.owner,
+                              style: decoration.collectionOwnerTextStyle,
+                            ),
+                            FutureBuilder<User?>(
+                              future: getUser(username: widget.collectionInfo.owner),
+                                builder: (context, snapshot) {
+                                  if(snapshot.hasData){
+                                    return CircleAvatar(
+                                      backgroundImage: NetworkImage(snapshot.data!.profilePicture),
+                                    );
+                                  }
+                                  else{
+                                    return CircleAvatar();
+                                  }
+
+
+                              }
+                            ),
+                          ],
+                        ),
+                      ),
+
+
+
+
+
+
+
+
+
+
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+
     );
   }
 }
